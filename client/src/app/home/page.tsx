09af00cc5ed1c5329908520eb7d6ce6c52314086
Anurag <anuragrajanp@gmail.com>
2023-11-12 00:39:39 +0530

    client(landing): hero

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -8,11 +8,10 @@ import { UploadButton } from "@/utils/uploadthing";
 import axios from "axios";
 import { serverUrl } from "@/utils/utils";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { AiFillSetting } from "react-icons/ai";
 
 export default function Home() {
-
 	const [valuators, setValuators] = useState([]);
 	const [valuation, setValuation] = useState([]);
 	const [title, setTitle] = useState("");
@@ -28,7 +27,7 @@ export default function Home() {
 			method: "GET",
 			url: `${serverUrl}/valuators`,
 			headers: {
-				"Authorization": `Bearer ${localStorage.getItem("token")}`
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
 			},
 		};
 
@@ -39,7 +38,7 @@ export default function Home() {
 			.catch((error) => {
 				toast.error("Failed to fetch valuators");
 			});
-	}
+	};
 
 	const createValuator = async () => {
 		setCreatingValuator(true);
@@ -47,32 +46,32 @@ export default function Home() {
 			method: "POST",
 			url: `${serverUrl}/valuators`,
 			headers: {
-				"Authorization": `Bearer ${localStorage.getItem("token")}`,
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
 				"Content-Type": `application/json`,
 			},
 			data: {
 				title: title,
 				questionPaper: questionPaperUrl,
 				answerKey: answerKeyUrl,
-			}
+			},
 		};
 
 		axios(config)
 			.then((response) => {
 				setCreatingValuator(false);
 				toast.success("Valuator created successfully!");
-				(document.getElementById("new_valuation_modal") as any).close()
+				(document.getElementById("new_valuation_modal") as any).close();
 			})
 			.catch((error) => {
 				setCreatingValuator(false);
 				toast.error("Error creating valuator!");
-				(document.getElementById("new_valuation_modal") as any).close()
+				(document.getElementById("new_valuation_modal") as any).close();
 			});
-	}
+	};
 
 	useEffect(() => {
 		getValuators();
-	}, [])
+	}, []);
 
 	return (
 		<>
@@ -90,9 +89,9 @@ export default function Home() {
 								<p className="font-semibold text-xl">New valuator</p>
 							</div>
 						</div>
-						{
-							valuators?.map((item: any, index: number) => {
-								return <div
+						{valuators?.map((item: any, index: number) => {
+							return (
+								<div
 									onClick={() => (document.getElementById("new_valuation_modal") as any).showModal()}
 									className="hover:shadow-2xl duration-100 cursor-pointer border-2 flex flex-col min-h-[400px] min-w-[350px] mb-10 mr-10 rounded-3xl shadow-lg overflow-hidden"
 								>
@@ -101,8 +100,8 @@ export default function Home() {
 										<p className="font-semibold text-xl">{item?.title}</p>
 									</div>
 								</div>
-							})
-						}
+							);
+						})}
 					</div>
 				</div>
 			</main>
@@ -114,46 +113,70 @@ export default function Home() {
 						<FiPlusCircle className="mr-2" /> Create new valuator
 					</h3>
 					<p className="mb-5 font-semibold">Exam title</p>
-					<input type="text" placeholder="Exam Title" className="input input-bordered w-full" value={title} onChange={(e) => setTitle(e.target.value)} />
+					<input
+						type="text"
+						placeholder="Exam Title"
+						className="input input-bordered w-full"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
 					<div className="w-full flex flex-col">
 						<p className="mb-2 mt-7 font-semibold">Upload question paper</p>
 						<div className="flex">
-							{questionPaperUrl ? questionPaperUrl : <UploadButton
-								endpoint="media"
-								onClientUploadComplete={(res) => {
-									// Do something with the response
-									console.log("Files: ", res![0].url);
-									setQuestionPaperUrl(res![0].url);
-								}}
-								onUploadError={(error: Error) => {
-									// Do something with the error.
-									alert(`ERROR! ${error.message}`);
-								}}
-							/>}
+							{questionPaperUrl ? (
+								questionPaperUrl
+							) : (
+								<UploadButton
+									endpoint="media"
+									onClientUploadComplete={(res) => {
+										// Do something with the response
+										console.log("Files: ", res![0].url);
+										setQuestionPaperUrl(res![0].url);
+									}}
+									onUploadError={(error: Error) => {
+										// Do something with the error.
+										alert(`ERROR! ${error.message}`);
+									}}
+								/>
+							)}
 						</div>
 					</div>
 					<div className="w-full flex flex-col">
 						<p className="mb-2 mt-7 font-semibold">Upload answer key / criteria</p>
 						<div className="flex">
-							{answerKeyUrl ? answerKeyUrl : <UploadButton
-								endpoint="media"
-								onClientUploadComplete={(res) => {
-									// Do something with the response
-									console.log("Files: ", res![0].url);
-									setAnswerKeyUrl(res![0].url);
-								}}
-								onUploadError={(error: Error) => {
-									// Do something with the error.
-									alert(`ERROR! ${error.message}`);
-								}}
-							/>}
+							{answerKeyUrl ? (
+								answerKeyUrl
+							) : (
+								<UploadButton
+									endpoint="media"
+									onClientUploadComplete={(res) => {
+										// Do something with the response
+										console.log("Files: ", res![0].url);
+										setAnswerKeyUrl(res![0].url);
+									}}
+									onUploadError={(error: Error) => {
+										// Do something with the error.
+										alert(`ERROR! ${error.message}`);
+									}}
+								/>
+							)}
 						</div>
 					</div>
-					<button className={"mt-10 btn w-full btn-primary " + ((!title || !questionPaperUrl || !answerKeyUrl) ? "opacity-50" : "")} onClick={() => {
-						if (!title || !questionPaperUrl || !answerKeyUrl) return;
-						createValuator();
-					}}>
-						{creatingValuator ? <span className="loading loading-spinner loading-sm"></span> : "Create Valuator"}
+					<button
+						className={
+							"mt-10 btn w-full btn-primary " +
+							(!title || !questionPaperUrl || !answerKeyUrl ? "opacity-50" : "")
+						}
+						onClick={() => {
+							if (!title || !questionPaperUrl || !answerKeyUrl) return;
+							createValuator();
+						}}
+					>
+						{creatingValuator ? (
+							<span className="loading loading-spinner loading-sm"></span>
+						) : (
+							"Create Valuator"
+						)}
 					</button>
 				</div>
 				<form method="dialog" className="modal-backdrop">
