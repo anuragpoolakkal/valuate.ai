c46289fce32ebc3104cff9a38b087d97c566b98e
Majid <majid616365@gmail.com>
2023-11-11 22:34:35 +0530

    f: pdf viewer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        
+        return config;
+     },
+    ...nextConfig
+     
+}
--- src/app/test/viewanswer/page.tsx
+++ src/app/test/viewanswer/page.tsx
@@ -1,46 +1,33 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
 
 const SampleTable = () => {
-  const [data, setData] = useState([
-    { id: 1, name: 'Cy Ganderton', rollNo: '23', marks: '234', isChecked: true },
-    { id: 2, name: 'Hart Hagerty', rollNo: '23', marks: '123', isChecked: false },
-    { id: 3, name: 'Brice Swyre', rollNo: '23', marks: '432', isChecked: true },
-    // Add more rows as needed
-  ]);
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
 
-  const handleCheckboxToggle = (id : number) => {
-    setData((prevData) =>
-      prevData.map((item) => (item.id === id ? { ...item, isChecked: !item.isChecked } : item))
-    );
-  };
-
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
   return (
-    <main className='h-full w-full p-10 bg-white'>
-    <div className="overflow-x-auto">
-      <table className="table table-zebra">
-        {/* head */}
-        <thead>
-          <tr className='text-center'>
-            <th></th>
-            <th>Name</th>
-            <th>Roll No.</th>
-            <th>Marks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* rows */}
-          {data.map((row) => (
-            <tr className='text-center'  key={row.id}>
-              <th>{row.id}</th>
-              <td>{row.name}</td>
-              <td>{row.rollNo}</td>
-              <td>{row.marks}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <main className="h-full w-full p-10 bg-white">
+      <div>
+
+      </div>
+      <div>
+        <Document file={"/assets/samplepdf.pdf"} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
     </main>
   );
 };
