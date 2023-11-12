e2190ed475b0dd7f4da825c33c0b3eac32bd09ac
Majid <majid616365@gmail.com>
2023-11-12 07:03:03 +0530

    f: update

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
-  
 
   return (
     <main className="flex items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tr from-[#6c63ff] to-[#20bee9]">
       <div className="backdrop-filter backdrop-blur-lg text-white">
         <div className="flex flex-col items-center text-white">
-          <h1 className="text-6xl font-extrabold mb-4 text-gradient">
-            Valuate.AI
+          <h1 className="text-6xl font-extrabold mb-4 text-gradient animate-fade-in">
+            GradeWiseAI
           </h1>
           <p className="text-2xl mb-8 text-opacity-80">
-            Exam Answer Paper Valuation and Marksheet Generation using AI
+            Elevate Education with GradeWiseAI: Transforming Traditional
+            Grading into Intelligent Assessment!
           </p>
           <button
             className="btn btn-primary btn-lg transform transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-primary-dark"
@@ -40,6 +40,21 @@ export default function Home() {
                 </li>
                 <li className="mb-2">⏰ Save time on answer paper valuation</li>
                 <li className="mb-2">📝 Review marks assigned by AI</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="card backdrop-filter backdrop-blur-lg bg-opacity-80 bg-base-100 border-b border-gray-200">
+            <div className="card-body text-black">
+              <h2 className="card-title text-3xl font-bold mb-5">
+                Generate marklists with ease
+              </h2>
+              <ul className="list-disc pl-6">
+                <li className="mb-2">📝 Generate marklists for each exam</li>
+                <li className="mb-2">📄 Export marklists as CSV files</li>
+                <li className="mb-2">
+                  📊 View overall statistics for each exam
+                </li>
               </ul>
             </div>
           </div>
